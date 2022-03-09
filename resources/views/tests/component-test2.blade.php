36cc5a91b5af84e7a576e6e0a52190f1096a0388
panpanYuya <yuya@sanbakaraisu.com>
2022-03-09 18:48:59 +0900

    classbaseの使い方

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,6 +3,9 @@
         ヘッダーだよ
     </x-slot>
     test2
+    <x-test-class-base classBaseMessage="テストのもの"></x-test-class-base>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="テストのもの" defaultMessage="変更が加わってることを確認してください。"></x-test-class-base>
 </x-tests.test>
 
 
