d61bc1bdc3b9113549a1ffa4533bf3912dbf1622
panpanYuya <yuya@sanbakaraisu.com>
2022-03-09 18:21:28 +0900

    componeSlotの練習

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,13 @@
-<div class="border-2 shadow-md w-1/4 p2">
+@props([
+    //すべての要素を書いてあげないとエラーになってしまうよ。
+    'title',
+    'content' => '初期値だよ',
+    'message' => 'これで表示がされるはず'
+])
+
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,7 @@
         ヘッダーかよ
     </x-slot>
     テスト1
-    <x-tests.card title="タイトル" content="本文です" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル" content="本文です" :message="$message" />
+    <x-tests.card title="タイトル2"  />
+    <x-tests.card title="タイトル3"  class="bg-red-300"/>
 </x-tests.test>
