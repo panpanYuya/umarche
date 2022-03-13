0aeddf9548def097fe9c0443458cfb2892b22cde
panpanYuya <yuya@sanbakaraisu.com>
2022-03-13 18:20:09 +0900

    マルチログイン機能のコミット

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
+
+    private const GUARD_USER = 'users';
+    private const GUARD_OWNER = 'owners';
+    private const GUARD_ADMIN = 'admin';
+
     /**
      * Handle an incoming request.
      *
@@ -19,14 +24,23 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        // $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        // foreach ($guards as $guard) {
+        //     if (Auth::guard($guard)->check()) {
+        //         return redirect(RouteServiceProvider::HOME);
+        //     }
+        // }
+
+        // return $next($request);
+        if(Auth::guard(self::GUARD_USER)->check() && $request->routeIs('users.*')){
+            return redirect(RouteServiceProvider::HOME);
         }
-
-        return $next($request);
+        if(Auth::guard(self::GUARD_OWNER)->check() && $request->routeIs('owners.*')){
+            return redirect(RouteServiceProvider::OWNER_HOME);
+        }
+        if(Auth::guard(self::GUARD_ADMIN)->check() && $request->routeIs('admin.*')){
+            return redirect(RouteServiceProvider::ADMIN_HOME);
+        }
     }
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
