475444f7e9cb3506d5fc127a647191bbbbbf1afd
panpanYuya <yuya@sanbakaraisu.com>
2022-03-25 11:23:50 +0900

    shopの追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Shop;
 
 
 class Owner extends Authenticatable
@@ -42,4 +43,8 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shop(){
+        return $this->hasOne(Shop::class);
+    }
 }
