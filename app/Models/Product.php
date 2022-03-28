f1af612a095d46992b46730c374805c99a82c6b1
panpanYuya <yuya@sanbakaraisu.com>
2022-03-28 21:49:09 +0900

    在庫機能リレーション設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 class Product extends Model
 {
@@ -28,6 +29,11 @@ class Product extends Model
     {
         //リレーション先はImageモデル
         return $this->belongsTo(Image::class, 'image1', 'id');
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
     }
 
 }
