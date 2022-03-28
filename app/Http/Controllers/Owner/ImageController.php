d05f5370d5fe228ebb7b25beb8c794956461910a
panpanYuya <yuya@sanbakaraisu.com>
2022-03-28 15:02:17 +0900

    写真削除機能

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
 
@@ -131,6 +132,22 @@ class ImageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/' . $image->fileName;
+
+        if(Storage::exists($filePath)){
+            Storage::delete($filePath);
+        }
+
+        Image::findOrFail($id)->delete();
+
+
+
+        return redirect()
+            ->route('owner.images.index')
+            ->with([
+                'message' => '画像を削除しました。',
+                'status' => 'alert',
+            ]);
     }
 }
