808495bd4db80879dec73fcb1dab1c125f25cfac
panpanYuya <yuya@sanbakaraisu.com>
2022-03-31 19:39:38 +0900

    商品ページを条件に合わせて表示できるようにする

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -14,7 +14,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Throwable;
 
 class ProductController extends Controller
