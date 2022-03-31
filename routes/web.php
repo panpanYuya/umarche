f3f9d03e349d95e9f816db486afb47f540a2d66a
panpanYuya <yuya@sanbakaraisu.com>
2022-03-31 18:20:21 +0900

    ユーザーのホーム画面ビュー作成

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ComponentTestController;
-use App\Http\Controllers\LifeCycleTestController;
+use App\Http\Controllers\User\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +14,25 @@ use App\Http\Controllers\LifeCycleTestController;
 |
 */
 
-Route::get('/', function () {
-    return view('user.welcome');
+// Route::get('/', function () {
+//     return view('user.welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
+
+Route::middleware('auth:users')->group(function () {
+    Route::get('/', [ItemController::class, 'index'])->name('items.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
 
-Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
-Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
-Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
-Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
+
+
+// Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+// Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+// Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
+// Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
+
 
 require __DIR__.'/auth.php';
