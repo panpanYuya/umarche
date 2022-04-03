7044c5d22e42b9723a73cdc26e4f7a3e0bb2e4e8
panpanYuya <yuya@sanbakaraisu.com>
2022-04-03 19:20:36 +0900

    テストメール送信を確認

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
 
@@ -29,6 +31,9 @@ class ItemController extends Controller{
     }
 
     public function index(Request $request){
+
+        Mail::to('test@example.com')
+        ->send(new TestMail());
 
         $categories = PrimaryCategory::with('secondary')->get();
 
